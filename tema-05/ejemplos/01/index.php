f18995fc63056d68721192b8b397a35cda176185
jroddom0103 <jroddom0103@g.educaand.es>
2024-11-22 13:05:21 +0100

    Añado

--- tema-05/ejemplos/01/index.php
+++ tema-05/ejemplos/01/index.php
@@ -17,4 +17,37 @@ $pass = '';
 $bd = 'fp';
 
 # establecemos la conexión
-$conexion = new PDO();
+$conexion = new mysqli($server, $user, $pass, $bd);
+
+# verificamos conexión
+if ($conexion->connect_error) {
+    echo 'ERROR DE CONEXIÓN Nº: ' . $conexion->connect_errno;
+    echo '<BR>';
+    echo 'DESCRIPCIÓN ERROR    : ' . $conexion->connect_errno;
+    exit();
+}
+
+echo "conexión establecida con éxito";
+echo "<BR>";
+
+# ejecuto sql
+$sql = 'select * from alumnos order by id';
+$result = $conexion->query($sql);
+
+# Manejo de resultado
+// $result objeto de clase mysqli_result
+
+# Muestro todos los alumnos
+while($alumno = $result->fetch_assoc()){
+    echo 'id: '.$alumno['id'];
+    echo '<BR>';
+    echo 'nombre: '.$alumno['nombre'];
+    echo '<BR>';
+    echo 'curso: '.$alumno['poblacion'];
+    echo '<BR>';
+    
+}
+
+# libero memoria y cierro conexión
+$result->free();
+$conexion->close();
