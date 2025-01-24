df5eb7fc984eef6513a52762b4ef4618dd637479
jroddom0103 <jroddom0103@g.educaand.es>
2025-01-24 14:37:50 +0100

    Añado

--- tema-07/proyectos/02-validacion-auth/01-validacion/template/partials/footer.partial.php
+++ tema-07/proyectos/02-validacion-auth/01-validacion/template/partials/footer.partial.php
@@ -4,7 +4,7 @@
 
 <footer class="footer mt-auto py-3 fixed-bottom bg-light">
   <div class="container">
-    <span class="text-muted">© 2021 
-        Juan Carlos Moreno - DWES - 2º DAW - Curso 24/25</span>
+    <span class="text-muted">© 2025 
+        Juan Antonio Rodríguez Domínguez - DWES - 2º DAW - Curso 24/25</span>
   </div>
 </footer>
---
+++ tema-07/proyectos/02-validacion-auth/01-validacion/views/auth/register/index.php
@@ -0,0 +1,104 @@
+<!doctype html>
+<html lang="es">
+
+<head>
+    <?php require_once 'template/layouts/head.layout.php'; ?>
+    <title>Registro Nuevo Usuario - Autenticación</title>
+</head>
+
+<body>
+    <!-- Menú fijo superior -->
+    <?php require_once 'template/partials/menu.partial.php' ?>
+
+    <!-- Capa Principal -->
+    <div class="container">
+        <br><br><br><br>
+
+        <?php require_once("template/partials/mensaje.partial.php") ?>
+        <?php require_once("template/partials/error.partial.php") ?>
+
+        <div class="card-header">Registro Nuevo Usuario</div>
+        <div class="card-body">
+            <form method="POST" action="<?= URL ?>auth/validate_register">
+                <!-- token csrf -->
+                <input type="hidden" name="csrf_token" value="<?= htmlspecialchars($_SESSION['csrf_token'] ?? '') ?>">
+
+                <!-- campo name -->
+                <div class="mb-3 row">
+                    <label for="name" class="col-md-4 col-form-label text-md-right">Nombre</label>
+                    <div class="col-md-6">
+                        <input id="name" type="text" class="form-control" name="name"
+                            value="<?= htmlspecialchars($this->name); ?>" required autocomplete="name" autofocus>
+                        <!-- control de errores -->
+                        <span class="form-text text-danger" role="alert">
+                            <?= $this->error['email'] ??= '' ?>
+                        </span>
+                    </div>
+                </div>
+
+                <!-- campo email -->
+                <div class="mb-3 row">
+                    <label for="email" class="col-md-4 col-form-label text-md-right">Email</label>
+
+                    <div class="col-md-6">
+                        <input id="email" type="email"
+                            class="form-control <?= (isset($this->errores['email'])) ? 'is-invalid' : null ?>"
+                            name="email" value="<?= htmlspecialchars($this->email); ?>" required autocomplete="email"
+                            autofocus>
+                        <!-- control de errores -->
+                        <span class="form-text text-danger" role="alert">
+                            <?= $this->error['email'] ??= '' ?>
+                        </span>
+                    </div>
+                </div>
+
+                <!-- campo password -->
+                <div class="mb-3 row">
+                    <label for="password" class="col-md-4 col-form-label text-md-right">Password</label>
+
+                    <div class="col-md-6">
+                        <input id="password" type="password"
+                            class="form-control <?= (isset($this->errores['password'])) ? 'is-invalid' : null ?>"
+                            name="password" value="<?= htmlspecialchars($this->password) ?>" required
+                            autocomplete="current-password">
+
+                        <!-- control de errores -->
+                        <span class="form-text text-danger" role="alert">
+                            <?= $this->error['email'] ??= '' ?>
+                        </span>
+                    </div>
+                </div>
+
+                <!-- password confirmación -->
+                <div class="mb-3 row">
+                    <label for="password-confirm" class="col-md-4 col-form-label text-md-right">Confirmar
+                        password</label>
+
+                    <div class="col-md-6">
+                        <input id="password-confirm" type="password" class="form-control" name="password" required
+                            autocomplete="new-password">
+                    </div>
+                </div>
+
+                <div class="mb-3 row mb-0">
+                    <div class="col-md-8 offset-md-4">
+                        <a class="btn btn-secondary" href="<?= URL ?>login" role="button">Cancelar</a>
+                        <button type="reset" class="btn btn-secondary">Reset</button>
+                        <button type="submit" formaction="<?= URL ?>register/validate"
+                            class="btn btn-primary">Registrar</button>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+
+    </div>
+
+    <!-- /.container -->
+
+    <?php require_once 'template/partials/footer.partial.php' ?>
+    <?php require_once 'template/layouts/javascript.layout.php' ?>
+
+</body>
+
+</html>
