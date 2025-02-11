8f5acf22fbcfec1cc14bb0591679f942e0c5b28c
jroddom0103 <jroddom0103@g.educaand.es>
2025-02-11 10:35:13 +0100

    Añado

---
+++ tema-08/proyectos/album/class/album.class.php
@@ -0,0 +1,50 @@
+<?php
+
+    /*
+        Creamos una clase para cada tabla
+        las propiedades públicas y una propiedad para cada columna
+
+        No respetará la propiedad de encapsulamiento.
+    */
+
+    class classAlbum {
+
+        public $id;
+        public $titulo;
+        public $descripcion;
+        public $autor;
+        public $fecha;
+        public $lugar;
+        public $categoria;
+        public $etiquetas;
+        public $num_fotos;
+        public $num_visitas;
+        public $carpeta;
+
+        public function __construct(
+            $id     = null,
+            $titulo = null,
+            $descripcion = null,
+            $autor = null,
+            $fecha = null,
+            $lugar = null,
+            $categoria = null,
+            $num_fotos = null,
+            $num_visitas = null,
+            $etiquetas = null,
+            $carpeta = null
+        ) {
+            $this->id = $id;
+            $this->titulo = $titulo;
+            $this->descripcion = $descripcion;
+            $this->autor = $autor;
+            $this->fecha = $fecha;
+            $this->lugar = $lugar;
+            $this->categoria = $categoria;
+            $this->num_fotos = $num_fotos;
+            $this->num_visitas = $num_visitas;
+            $this->etiquetas = $etiquetas;
+            $this->carpeta = $carpeta;
+        }
+        
+    }
---
+++ tema-08/proyectos/album/config/config.php
@@ -0,0 +1,16 @@
+<?php
+# Configuración básica aplicación MVC
+
+# Ruta absoluta
+
+define('URL', 'http://localhost:8080/DWES/tema-08/proyectos/album/');
+
+# Constante de la Base de Datos
+define('HOST', 'localhost');
+define('DB', 'album');
+define('USER', 'root');
+define('PASSWORD', '');
+define('CHARSET', 'utf8mb4');
+
+
+?>
